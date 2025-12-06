b6cb8f06e1209a4c9b25bf2a75681ff7360a252a
Aditya Bhanu <adityabhanu08@gmail.com>
2025-12-06 20:03:51 +0530

    base route for github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Memorial from "./pages/Memorial";
 import Cemetery from "./pages/Cemetery";
@@ -6,13 +6,15 @@ import PageLayout from "./layout/PageLayout";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<PageLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/memorial" element={<Memorial />} />
-        <Route path="/cemetery" element={<Cemetery />} />
-      </Route>
-    </Routes>
+    <BrowserRouter basename="/rise">
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/memorial" element={<Memorial />} />
+          <Route path="/cemetery" element={<Cemetery />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
